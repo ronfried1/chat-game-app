b44eacbcc920ed32f80e53a33b8731a0b8cf99ea
ron <ron.fried15@gmail.com>
2023-09-04 20:41:54 +0300

    adding build folder

---
+++ client/build/precache-manifest.ab1b0171d881afa4a8b5a5b41fd373b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0617e1cc4704e77b10d6b66685c77278",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7e4c6e04627f9bf33f0",
+    "url": "/static/css/main.bea4d84e.chunk.css"
+  },
+  {
+    "revision": "26e5b3c3ec861dd4a39a",
+    "url": "/static/js/2.85b3a881.chunk.js"
+  },
+  {
+    "revision": "e7e4c6e04627f9bf33f0",
+    "url": "/static/js/main.79be7760.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
